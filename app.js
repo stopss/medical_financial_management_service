c671ee3cde81c663693d3b200c52bdc8085ad228
jieun <stopss417@gmail.com>
2023-02-24 15:06:54 +0900

    Database Setting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
-
+const { sequelize } = require('./models');
 dotenv.config();
 
 const app = express();
@@ -14,12 +14,22 @@ nunjucks.configure('views', {
     watch: true,
 });
 
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '포트에서 대기 중');
